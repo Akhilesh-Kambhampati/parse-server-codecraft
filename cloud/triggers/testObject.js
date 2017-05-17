4289436ae90f8f8af2870690b822a36ba00abbdc
Akhilesh <Akhilesh-Kambhampati@users.noreply.github.com>
2017-05-17 11:26:58 -0400

    Update testObject.js

--- cloud/triggers/testObject.js
+++ cloud/triggers/testObject.js
@@ -1,6 +1,6 @@
 Parse.Cloud.beforeSave("TestObject", function(request, response) {
-  if (!request.object.get("foo")) {
-    response.error("foo must be set");
+  if (!request.object.get("contact")) {
+    response.error(" please enter a phone number for signup");
   } else {
     response.success();
   }
